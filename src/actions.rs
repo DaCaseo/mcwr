a11fa5fb9bae311a201a40f05475da87310e78c1
DaCaseo <34115435+DaCaseo@users.noreply.github.com>
2020-07-21 16:09:46 -0500

    changed from tmux to duct for commands

--- src/actions.rs
+++ src/actions.rs
@@ -1,17 +1,37 @@
-pub fn start_server(args: &super::Args, mut tmux: tmux_interface::TmuxInterface) {
-	let new_session = tmux_interface::NewSession {
-		cwd: Some("./server"),
-		session_name: Some(&args.name),
-		shell_command: Some("java -Xms1G -Xmx3G -jar ./server.jar nogui"),
-		..Default::default()
-	};
-	tmux.new_session(Some(&new_session)).expect("failed to create session");
+pub fn start_server(_name: &String) {
+	use duct::cmd;
+	use std::io::{prelude::*, BufReader, stdout, Write};
+	std::env::set_current_dir("server").unwrap();
+	let reader = cmd!("java", "-jar", "server.jar", "nogui").reader().unwrap();
+
+	let buf_reader = BufReader::new(reader);
+
+	for l in buf_reader.lines() {
+		let line = l.unwrap() + &String::from("\n");
+		let mut stdout = stdout();
+		stdout.write(line.as_bytes()).unwrap(); // print each stdin character to stdout to give user feedback when typing
+	}
 }
 
-pub fn stop_server(args: &super::Args, mut tmux: tmux_interface::TmuxInterface) {
-	 let stop_session = tmux_interface::SendKeys {
-		 target_pane: Some(&args.name),
-		 ..Default::default()
-	 };
-	 tmux.send_keys(Some(&stop_session), &vec!("stop", "Enter")).expect("failed to send stop");
+pub fn stop_server(_name: &String) { // use std::env::set_current_dir();
+	
 }
+
+/* pub fn start_server(name: &String) {
+	use std::process::{Command, Stdio};
+	use std::io::{Write, Read};
+	let child = Command::new("java")
+		.arg("-jar")
+		.arg("server.jar")
+		.arg("nogui")
+		.stdout(Stdio::piped())
+		.current_dir(std::path::PathBuf::from("/home/dacaseo/Documents/mcwr/server/"))
+    	.spawn()
+		.expect("Failed to execute command");
+	
+	let mut stdout_string = String::new();
+	child.stdout.expect("failed to open stdout of child").read_to_string(&mut stdout_string).unwrap();
+	let mut stdout = std::io::stdout();
+	
+	stdout.write(&stdout_string.as_bytes()).unwrap();
+} */
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use structopt::StructOpt;
 use mcwr::*;
-use tmux_interface::TmuxInterface;
 
 fn main() {
 	let args = Args::from_args();
-	let tmux = TmuxInterface::new();
 
 	match &args.action[..] {
-		"start" => actions::start_server(&args, tmux),
-		"stop" => actions::stop_server(&args, tmux),
+		"start" => actions::start_server(&args.name),
+		"stop" => actions::stop_server(&args.name),
 		_ => println!("\"{}\" is not a command", args.name)
 	}
 }
